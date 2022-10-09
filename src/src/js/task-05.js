f517c81b6c431765575fbbe75858951fb77f09ab
IvanHolovakha <meisuiwanreiser@gmail.com>
2022-10-09 21:34:55 +0300

    add src

---
+++ src/src/js/task-05.js
@@ -0,0 +1,11 @@
+const inputRef = document.querySelector("#name-input");
+const outputRef = document.querySelector("#name-output");
+
+inputRef.addEventListener("input", onInputChange);
+
+function onInputChange(event) {
+    outputRef.textContent = event.currentTarget.value.trim();
+    if (event.currentTarget.value === "") {
+        outputRef.textContent = "Anonymous"
+    }
+}
